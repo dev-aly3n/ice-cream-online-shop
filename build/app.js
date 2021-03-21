0b60ba609ac79e22fa0527c47c6b9be227111775
aly3n-dev <aly3n.dev@gmail.com>
2021-03-22 03:24:12 +0430

    this is the modern navBar via a bug that is overflow problem.

---
+++ build/app.js
@@ -0,0 +1,31 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    //toggle nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active')
+
+        //animate links
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.6}s`;
+            }
+
+        });
+
+        //burger animation 
+        burger.classList.toggle('toggle');
+
+    });
+
+
+
+}
+
+navSlide();
